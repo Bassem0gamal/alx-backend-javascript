744891c727d2244d05981eda1a48cc89f8786864
Bassem Gamal <bassemgamalosman12@gmail.com>
2024-08-08 00:06:12 +0300

    Task 6 : Fix

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,10 +1,10 @@
 import SignupUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup([firstName, lastName, filename]) {
-  return Promise.all([SignupUser(firstName, lastName), uploadPhoto(filename)])
+export default function handleProfileSignup(firstName, lastName, filename) {
+  return Promise.allSettled([SignupUser(firstName, lastName), uploadPhoto(filename)])
     .then((values) => values.map((result) => ({
       status: result.status,
-      value: result.status === 'fulfilled' ? result.value : result.reason,
+      value: result.status === 'fulfilled' ? result.value : (result.reason),
     })));
 }
