202a581c317733c8bb15a5f53e4e97497aad00ef
Bassem Gamal <bassemgamalosman12@gmail.com>
2024-08-09 03:31:44 +0300

    Fix Task 2

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,5 +1,17 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+
+    if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+
+    if (!Array.isArray(students)) {
+      throw new TypeError('Students must be an array');
+    }
+
     this._name = name;
     this._length = length;
     this._students = students;
@@ -9,32 +21,32 @@ export default class HolbertonCourse {
     return this._name;
   }
 
-  set name(value) {
-    if (typeof value !== 'string') {
+  set name(str) {
+    if (typeof str !== 'string') {
       throw new TypeError('Name must be a string');
     }
-    this._name = value;
+    this._name = str;
   }
 
   get length() {
     return this._length;
   }
 
-  set length(value) {
-    if (typeof value !== 'number') {
+  set length(num) {
+    if (typeof num !== 'number') {
       throw new TypeError('Length must be a number');
     }
-    this._length = value;
+    this._length = num;
   }
 
   get students() {
     return this._students;
   }
 
-  set students(value) {
-    if (!Array.isArray(value)) {
+  set students(arr) {
+    if (!Array.isArray(arr)) {
       throw new TypeError('Students must be an array');
     }
-    this._students = value;
+    this._students = arr;
   }
 }
