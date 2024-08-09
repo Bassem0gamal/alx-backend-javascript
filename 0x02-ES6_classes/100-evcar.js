a36a6f6053cb115c88444809ccf4b78ecbbcce53
Bassem Gamal <bassemgamalosman12@gmail.com>
2024-08-09 05:09:17 +0300

    Task 11 : EVCar

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,16 @@
+import Car from './10-car';
+
+export default class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this.range = range;
+  }
+
+  // eslint-disable-next-line consistent-return
+  cloneCar() {
+    if (this._color) {
+      return new Car();
+    }
+    return new Car();
+  }
+}
