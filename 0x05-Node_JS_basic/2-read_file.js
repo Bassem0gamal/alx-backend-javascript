ce89f65a63cc35a8f9906482237420ba70147171
Bassem Gamal <bassemgamalosman12@gmail.com>
2024-10-02 01:32:22 +0300

    Task 2: Reading a file synchronously with Node JS

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+
+module.exports = function countStudents(path) {
+  try {
+    const data = fs.readFileSync(path, { encoding: 'utf8' });
+    const lines = data.split('\n').filter((line, index) => index !== 0 && line !== '').map((line) => line.split(','));
+    const students = lines.map((line) => ({ name: line[0], field: line[3] }));
+    const fields = Array.from(new Set(lines.map((line) => line[3])));
+    console.log(`Number of students: ${students.length}`);
+    fields.forEach((field) => {
+      const fieldStudents = Array.from(new Set(students.filter(
+        (student) => student.field === field,
+      ).map((student) => student.name)));
+      console.log(`Number of students in ${field}: ${fieldStudents.length}. List: ${fieldStudents.join(', ')}`);
+    });
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+};
